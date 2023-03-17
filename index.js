9ca67ed5cad5096dc9a1d58e60a8af9596edd31a
otienosteve <steveotieno701@gmail.com>
2023-03-17 09:39:39 +0300

    Complete Assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers=(array)=>{
+    return array.slice(0,2)
+}
+//["elem1", "elem2", "elem3", "elem4","elem5"]
+const returnLastTwoDrivers=(array)=>{
+    return array.slice(array.length-2)
+}
+let selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+let fareMultiplier=(fare)=>{
+    return fare*num
+}
+return fareMultiplier
+}
+
+const fareDoubler=createFareMultiplier(2)
+const fareTripler=createFareMultiplier(3)
+
+
+function selectDifferentDrivers(arrayOfDrivers,functionPlaceholder){
+
+
+let drivers=functionPlaceholder(arrayOfDrivers)
+return drivers
+
+}
+
+// function square(num){
+//     return num*num
+// }
+// square(4)
+// square(7)
+// square(12)
+// square(20000)
+
+
+
+
+
+
+
+
+
+
+
+
